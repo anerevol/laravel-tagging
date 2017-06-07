8e2882c12823c0a02e666e9f8e3d627d821f358c
anerevol <anerevol@gmail.com>
2017-06-07 11:04:42 +0800

    add migrations

---
+++ migrations/2017-06-07_073615_update_tags_table_model_can_multible_tag.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anerevol
+ * Date: 07/06/2017
+ * Time: 11:01 AM
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class UpdateTagsTable extends Migration {
+
+    public function up()
+    {
+
+        Schema::table('tagging_tags', function ($table) {
+            $table->integer('count')->unsigned()->default(1);
+        });
+
+        Schema::table('tagging_tag_groups', function ($table) {
+            $table->integer('display_format')->unsigned()->default(1);
+        });
+
+    }
+
+
+    public function down()
+    {
+        Schema::table('tagging_tags', function ($table) {
+            $table->dropColumn('count');
+        });
+
+        Schema::table('tagging_tag_groups', function ($table) {
+            $table->dropColumn('display_format');
+        });
+    }
+}
