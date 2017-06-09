cd2827d87c3ca428b2ff4f5ef1a8a64390b3f417
anerevol <anerevol@gmail.com>
2017-06-09 11:24:50 +0800

    change group display format to string

---
+++ migrations/2017-06-09_073615_update_tag_groups_table_display_format.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anerevol
+ * Date: 07/06/2017
+ * Time: 11:01 AM
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class TableType extends Migration {
+
+    public function up()
+    {
+
+        Schema::table('tagging_tag_groups', function ($table) {
+            $table->string('display_format')->nullable()->change();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::table('tagging_tags', function ($table) {
+            $table->integer('display_format')->nullable()->change();
+        });
+    }
+}
